a4e7fede6861c1ddd38e518f43dd51823ace36ac
Stefano Zavatta <stefano@pstudios.it>
2022-11-09 21:25:12 +0100

    Add project files.

---
+++ AppPalestre/Controllers/HomeController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AppPalestre.Models;
+using Newtonsoft.Json.Linq;
+
+namespace AppPalestre.Controllers
+{
+    public class HomeController : Controller
+    {
+        public class Giorno
+        {
+            public DateTime Data { get; set; }
+            public string Datas { get; set; }
+            public List<Corso> Corsi { get; set; }
+        }
+
+        public class Corso
+        {
+            public int Id { get; set; }
+            public string Nome { get; set; }
+            public string Inizio { get; set; }
+            public string Fine { get; set; }
+            public bool IsPrenotato { get; set; }
+        }
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            PalestreApi api = new PalestreApi();
+            JObject pal = api.Palinsesti();
+
+            List<Giorno> giorni = null;
+
+            if ((string)pal["status"] == "2")
+            {
+                giorni = new List<Giorno>();
+                foreach (JToken giorno in (JArray)pal.SelectToken("$..lista_risultati..giorni"))
+                {
+                    DateTime data = Convert.ToDateTime(giorno.SelectToken("giorno"));
+                    Giorno g = new Giorno { Data = data, Datas = giorno.SelectToken("nome_giorno").ToString() };
+                    g.Corsi = new List<Corso>();
+                    foreach (JToken orario in (JArray)giorno.SelectToken("orari_giorno"))
+                    {
+                        g.Corsi.Add(new Corso
+                        {
+                            Id = (int)orario["id_orario_palinsesto"],
+                            Nome = (string)orario["nome_corso"],
+                            Inizio = (string)orario["orario_inizio"],
+                            Fine = (string)orario["orario_fine"],
+                            IsPrenotato = (string)orario["prenotazioni"]["utente_prenotato"] != "0"
+                        });
+                    }
+                    giorni.Add(g);
+                }
+                ViewBag.Giorni = giorni;
+            }
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public JsonResult Prenota(int idcorso, DateTime? datacorso)
+        {
+            PalestreApi api = new PalestreApi();
+            string id = api.Prenota(idcorso, datacorso?.ToString("yyyy-MM-dd"));
+            
+            return Json(id);
+        }
+
+        public JsonResult Elimina(int idprenotazione)
+        {
+            PalestreApi api = new PalestreApi();
+            bool ret = api.Elimina(idprenotazione);
+
+            return Json(ret);
+        }
+
+    }
+}
---
+++ AppPalestre/PalestreApi.cs
@@ -0,0 +1,115 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AppPalestre
+{
+    public class PalestreApi
+    {
+        public JObject Palinsesti()
+        {
+            var request = (HttpWebRequest)WebRequest.Create("https://app.shaggyowl.com/funzioniapp/v405/palinsesti");
+
+            var postData = $"id_sede=8628&codice_sessione=mfAQXc4rOBOq4twO3CaO&giorno={DateTime.Now.ToString("yyyy-MM-dd")}";
+            var data = Encoding.ASCII.GetBytes(postData);
+
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            JObject obj = JObject.Parse(responseString);
+
+            return obj;
+        }
+
+        public string Prenota(int idcorso, string datacorso)
+        {
+            var request = (HttpWebRequest)WebRequest.Create("https://app.shaggyowl.com/funzioniapp/v405/prenotazione_new");
+
+            var postData = $"id_sede=8628&codice_sessione=mfAQXc4rOBOq4twO3CaO&id_orario_palinsesto={idcorso}&data={datacorso}";
+            var data = Encoding.ASCII.GetBytes(postData);
+
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            JObject obj = JObject.Parse(responseString);
+
+            string ret = null;
+            if ((string)obj["status"] == "2")
+            {
+                ret = (string)obj["status"]["parametri"]["prenotazione"]["id_prenotazione"];
+            }
+
+            return (ret);
+        }
+
+        public bool Elimina(int idprenotazione)
+        {
+            var request = (HttpWebRequest)WebRequest.Create("https://app.shaggyowl.com/funzioniapp/v405/cancella_prenotazione");
+
+            var postData = $"id_sede=8628&codice_sessione=mfAQXc4rOBOq4twO3CaO&id_prenotazione={idprenotazione}&tipo=prenotazione";
+            var data = Encoding.ASCII.GetBytes(postData);
+
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            JObject obj = JObject.Parse(responseString);
+
+            bool ret = (string)obj["status"] == "2";
+
+            return (ret);
+        }
+
+    }
+
+
+    public static class GenreRatingFinder
+    {
+        public static (string? Genre, double Imdb, double Rotten) UsingDynamic(string jsonString)
+        {
+            var dynamicObject = JsonConvert.DeserializeObject<dynamic>(jsonString)!;
+            var genre = dynamicObject.Genre;
+            var imdb = dynamicObject.Rating.Imdb;
+            var rotten = dynamicObject.Rating["Rotten Tomatoes"];
+            return (genre, imdb, rotten);
+        }
+    }
+
+
+}
